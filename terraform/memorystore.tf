c9ef135f8433043005bdf8e719bb2fcce077334d
last-devops <68482383+last-devops@users.noreply.github.com>
2024-11-20 23:19:20 +0000

    removing snake case

--- terraform/memorystore.tf
+++ terraform/memorystore.tf
@@ -31,7 +31,7 @@ resource "google_redis_instance" "this" {
 }
 
 # Edit contents of Memorystore kustomization.yaml file to target new Memorystore (redis) instance
-resource "null_resource" "kustomization-update" {
+resource "null_resource" "kustomization_update" {
   provisioner "local-exec" {
     interpreter = ["bash", "-exc"]
     command     = "sed -i \"s/REDIS_CONNECTION_STRING/${google_redis_instance.this[0].host}:${google_redis_instance.this[0].port}/g\" ../kustomize/components/memorystore/kustomization.yaml"
